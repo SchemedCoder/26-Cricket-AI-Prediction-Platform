3beae3322f2f4b9beb11c5180b734f273edc0789
Navin Tigga <146591653+SchemedCoder@users.noreply.github.com>
2026-06-27 00:29:01 +0530

    Update test_prediction.py

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -1,7 +1,12 @@
+import joblib
 import pandas as pd
 
 
-def test_sample_input_schema():
+def test_prediction_output():
+
+    winner_model = joblib.load("ml/winner_model.pkl")
+    score_model = joblib.load("ml/score_model.pkl")
+
     sample = pd.DataFrame([{
         "team1_encoded": 3,
         "team2_encoded": 2,
@@ -9,9 +14,8 @@ def test_sample_input_schema():
         "team1_bat_first": 1
     }])
 
-    assert sample.shape == (1, 4)
+    winner = winner_model.predict(sample)[0]
+    score = score_model.predict(sample)[0]
 
-
-def test_valid_prediction_range():
-    predicted_score = 188
-    assert 50 <= predicted_score <= 300
+    assert winner in [0, 1]
+    assert score > 0
